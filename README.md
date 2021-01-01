2f8b9790ef9a22083a7c3e6378797caa75018f40
wowwish <vavish.ram@gmail.com>
2021-01-01 15:03:55 +0530

    created repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# Guessing Game
+Date and Time of Run : Friday 01 January 2021 03:01:32 PM IST
+Number of Lines of Code (guessinggames.sh) : 37
---
+++ guessinggame.sh
@@ -0,0 +1,37 @@
+#!/use/bin/env bash
+#File Name : guessinggame.sh
+
+let target=$(ls -l . | egrep '^-' | wc -l)
+
+function validate {
+	if [[ $1 =~ [^0-9] ]]
+	then
+		 local flag=0
+	else
+		 local flag=1
+	fi
+	echo $flag
+}
+
+while [[ $target -gt 0 ]]
+do
+	echo "Guess the number of files : "
+	read response
+	if [[ $(validate $response) -eq 0 ]]
+	then
+		echo "You entered an Invalid number!"
+		echo ""
+		continue
+	elif [[ $response -lt $target ]]
+	then
+		echo "Your guess is too low."
+	elif [[ $response -gt $target ]]
+	then
+		echo "Your guess is too high."
+	elif [[ $response -eq $target ]]
+	then
+		echo "Congratulations - your Guess is right."
+		break
+	fi
+	echo ""
+done
